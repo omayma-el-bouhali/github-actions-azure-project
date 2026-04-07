58c9f6a2eae060a5fa553008e3a54530d4ec87a6
omayma-bou <omaymaelbouhali2004@gmail.com>
2026-04-07 23:19:43 +0200

    All files ready for Azure

---
+++ server.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+let counter = 0;
+
+app.get('/', (req, res) => {
+  counter++;
+  res.send("Visitor count: " + counter);
+});
+
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
